ec7ade4abcbf0b5b4f5b718813b8a2a54f12ea77
Yehor <Y.miller@altec-tp.com>
2023-05-01 00:28:34 +0200

    set up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,47 @@
 const gulp = require('gulp');
+
 const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+// const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+
+// function script() {
+//     return gulp.src('app/js/*.js')
+//         .pipe(concat('main.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('app/js'))
+//         .pipe(browserSync.stream());
+
+// }
 
 function styles() {
     return gulp.src('app/scss/style.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('app/css'));
+        .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 };
 
+function watching() {
+    gulp.watch(['app/scss/style.scss'], styles)
+    gulp.watch(['app/index.html']).on('change', browserSync.reload);
+    // gulp.watch(['app/js/script.js'], scripts)
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+
 exports.styles = styles;
+// exports.script = script;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = gulp.parallel(styles, browsersync, watching)
